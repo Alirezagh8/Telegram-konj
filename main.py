811d5495b49b93b143b2b7606b9e49e9bf7d989d
Alirezagh8 <109094678+Alirezagh8@users.noreply.github.com>
2025-05-08 19:53:20 +0330

    main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(f"سلام {update.effective_user.first_name}! من همیشه آنلاینم 😊")
 
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("7875933982:AAF_gSJjC2ma2OXUkRuwN9TL3aHgKpA8juQ")
 
 app = ApplicationBuilder().token(TOKEN).build()
 
